020b4771c82302f497ce6db069bd34ed4fd29985
kongjs <kongjs24@daum.net>
2014-04-12 15:35:40 +0900

    springinaction

---
+++ springinaction/src/main/java/net/daum/kong/springinaction/App.java
@@ -0,0 +1,28 @@
+package net.daum.kong.springinaction;
+
+public class App {
+	private static final String CONSTANT = "String";
+	private Object o;
+
+	/**
+	 * Creates a new demo.
+	 * 
+	 * @param o
+	 *            The object to demonstrate.
+	 */
+	public App() {
+
+	}
+
+	public App(Object o) {
+		this.o = o;
+		String s = CONSTANT;
+		int i = 1;
+	}
+
+	public static void main(String[] args) {
+		App demo = new App();
+		for (int i = 0; i < 10; i++)
+			System.out.println("hello world");
+	}
+}
